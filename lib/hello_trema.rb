c60255204457e580e6803c11642984d58419ec79
AiharaMasahiko <aihara.masahiko@osaka-u.ac.jp>
2015-10-14 15:45:45 +0900

    スイッチ停止時のコメントとクラス名の表示を追加、レポートの作成

--- lib/hello_trema.rb
+++ lib/hello_trema.rb
@@ -2,9 +2,15 @@
 class HelloTrema < Trema::Controller
   def start(_args)
     logger.info 'Trema started.'
+    logger.info "Hi #{self.class}!"
   end
 
   def switch_ready(datapath_id)
     logger.info "Hello #{datapath_id.to_hex}!"
   end
+
+  def switch_disconnected(datapath_id)
+    logger.info "Goodbye #{datapath_id.to_hex}!"
+  end
+
 end
---
+++ report.md
@@ -0,0 +1,12 @@
+課題1
+スイッチを停止するとメッセージを表示
+
+def switch_disconnected(_args)を定義し、
+その中で開始時と同様にスイッチのidを表示した。
+
+課題2
+HelloTremaが起動したらクラス名を表示
+
+クラス名はself.classを用いれば呼び出すことができるので、
+logger.info "Hi #{self.class}!"の文を追加することで
+クラス名を表示することができた。
